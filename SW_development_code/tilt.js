f38e9a652b18ad1701a5236122bf69963e816d7f
Mingyeong-Kim <65681568+Mingyeong-Kim@users.noreply.github.com>
2021-10-23 01:21:57 +0900

    Create tilt.js
    기울기 센서 처리 코드

---
+++ SW_development_code/tilt.js
@@ -0,0 +1,26 @@
+// 기울기 센서 함수1
+// 데이터 받기
+msg.payload=msg.payload.d.inclination;
+
+global.set("inclination",msg.payload);
+return msg;
+
+// 기울기 센서 함수2
+// 데이터 처리
+var inc = global.get("inclination");
+var state = "on";
+     //미래  현재
+     
+if( (inc == "0") ){
+    state = "unblock";
+}
+else{
+    state="block";
+}
+
+
+
+msg.payload = inc;
+global.set("inc_state",state);
+
+return msg;
