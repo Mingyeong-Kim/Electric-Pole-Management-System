1ed02ffa576129b538f56eb72b2d3529942ead4a
Mingyeong-Kim <65681568+Mingyeong-Kim@users.noreply.github.com>
2021-10-23 01:22:52 +0900

    기울기 센서 처리 코드
    기울기 센서 처리 코드

--- SW_development_code/tilt.js
+++ SW_development_code/tilt.js
@@ -1,12 +1,12 @@
 // 기울기 센서 함수1
-// 데이터 받기
+// 데이터 받기 
 msg.payload=msg.payload.d.inclination;
 
 global.set("inclination",msg.payload);
 return msg;
 
 // 기울기 센서 함수2
-// 데이터 처리
+// 데이터 처리 및 상태판단
 var inc = global.get("inclination");
 var state = "on";
      //미래  현재
